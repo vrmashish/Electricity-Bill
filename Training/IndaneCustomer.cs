60ef4856deddfe7bd565d259ae8f53f25ae929cc
Ashish Verma <vrm.ashish@outlook.com>
2020-10-07 13:04:32 +0530

    IndaneCustomer

---
+++ Training/IndaneCustomer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+public class IndaneCustomer
+{
+    int customerid;
+    string name;
+    float unitconsumed;
+    float billamaount;
+    float netbillamaount;
+    float charge;
+
+    void calculate_bill()
+    {
+        if (unitconsumed <= 199)
+        {
+            charge = (float)1.20;
+        }
+        else if (unitconsumed > 199 && unitconsumed < 400)
+        {
+            charge = (float)1.50;
+        }
+        else if (unitconsumed > 399 && unitconsumed < 600)
+        {
+            charge = (float)1.80;
+        }
+        else if (unitconsumed > 599)
+        {
+            charge = (float)2.00;
+        }
+        billamaount = (unitconsumed * charge);
+
+        if (billamaount >= 400)
+        {
+            netbillamaount = (float)(billamaount + (0.15 * billamaount));
+        }
+        else if (billamaount < 400)
+        {
+            netbillamaount = billamaount;
+        }
+    }
+
+    public void getdata()
+    {
+        Console.WriteLine("Enter the Customer ID");
+        customerid = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Name");
+        name = Console.ReadLine();
+        Console.WriteLine("Enter the Units Consumed");
+        unitconsumed = (float)Convert.ToInt32(Console.ReadLine());
+        calculate_bill();
+
+    }
+
+    public void showdata()
+    {
+        Console.WriteLine("Amount Charges @Rs.{0} per unit : Rs. {1}", charge, billamaount);
+        if (unitconsumed > 400)
+        {
+            Console.WriteLine("Surcharge Amount above 400 units : 240.00");
+        }
+        Console.WriteLine("Net amount paid by the customer : Rs. {0}", netbillamaount);
+
+    }
+}
--- Training/Program.cs
+++ Training/Program.cs
@@ -1,64 +1,11 @@
 ﻿using System;
-class IndaneCustomers
-{
-    int customerid;
-    string name;
-    float unitconsumed;
-    float billamaount;
-    float charge;
-
-    void calculate_bill()
-    {
-        if (unitconsumed <= 199)
-        {
-            charge = (float)1.20;
-            billamaount = (unitconsumed * charge);
-        }
-        else if (unitconsumed > 199 && unitconsumed < 400)
-        {
-            charge = (float)1.50;
-            billamaount = (unitconsumed * charge);
-
-        }
-        else if (unitconsumed > 399 && unitconsumed < 600)
-        {
-            charge = (float)1.80;
-            billamaount = (unitconsumed * charge);
-        }
-        else if (unitconsumed > 599)
-        {
-            charge = (float)2.00;
-            billamaount = (unitconsumed * charge);
-        }
-
-        Console.WriteLine("Amount Charges @Rs.{0} per unit : {1}",charge, billamaount);
-        if (billamaount >= 400)
-        {
-            billamaount = (float)(billamaount + (0.15 * billamaount));
-            Console.WriteLine("Surcharge Amount : 240.00");
-        }
-        
-        Console.WriteLine("Net amount paid by the customer : {0}", billamaount);
-
-    }
-
-    public void getdata()
-    {
-        Console.WriteLine("Enter the Customer ID");
-        customerid = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter the Name");
-        name = Console.ReadLine();
-        Console.WriteLine("Enter the Units Consumed");
-        unitconsumed = (float)Convert.ToInt32(Console.ReadLine());
-        calculate_bill();
-
-    }
-}
 class Program
 {
     static void Main()
     {
-        IndaneCustomers s = new IndaneCustomers();
+        IndaneCustomer s = new IndaneCustomer();
         s.getdata();
+        s.showdata();
+        
     }
 }
